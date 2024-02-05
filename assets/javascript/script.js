d20d25df83c12099404e00ba30564d9fdbb2ce52
Balzabu <45636060+Balzabu@users.noreply.github.com>
2024-02-05 21:14:00 +0100

    Update script.js
    Added 3h Speedups support.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,238 +1,139 @@
-/* Base styles */
-body {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    background-color: #111;
-    color: #ccc;
-    margin: 0;
-    padding: 0;
-}
-
-header {
-    background-color: #222;
-    padding: 20px;
-    text-align: center;
-}
-
-.logo img {
-    width: 50px;
-    height: 50px;
-    vertical-align: middle;
-}
-
-.logo-text {
-    display: inline-block;
-    vertical-align: middle;
-    margin-left: 10px;
-    font-size: 24px;
-    color: #fff;
-}
-
-.container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-    min-height: calc(100vh - 120px); /* Minimum height for main content container */
-}
-
-h1 {
-    text-align: center;
-    color: #fff;
-    font-size: 28px;
-}
-
-.tab {
-    overflow-x: auto; /* Enable horizontal scrolling */
-    white-space: nowrap; /* Prevent tab buttons from wrapping */
-    background-color: #333;
-    display: flex;
-    justify-content: center;
-    padding-left: 20px; /* Add padding to the left side */
-    padding-right: 20px;/* Add padding to the right side */
-}
-
-.tab button {
-    background-color: #333;
-    color: #ccc;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    padding: 14px 16px;
-    font-size: 16px;
-    transition: background-color 0.3s ease;
-    flex-shrink: 0; /* Ensure button size remains fixed */
-}
-
-.tab button:hover {
-    background-color: #555;
-}
-
-.tab button.active {
-    background-color: #555;
-}
-
-.tabcontent {
-    display: none;
-    padding: 20px;
-    border-top: none;
-    background-color: #333;
-}
-
-.button-container {
-    text-align: center;
-    margin-top: 20px;
-}
-
-.button-container button {
-    background-color: #555;
-    color: #fff;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    padding: 10px 20px;
-    margin: 5px;
-    border-radius: 5px;
-    transition: background-color 0.3s ease;
-}
-
-.button-container button:hover {
-    background-color: #777;
-}
-
-.input-group {
-    margin-bottom: 10px;
-}
-
-input[type="number"] {
-    width: 60px;
-    padding: 8px;
-    border: 1px solid #555;
-    border-radius: 5px;
-    background-color: #333;
-    color: #ccc;
-    font-size: 16px;
-}
-
-input[type="number"]:focus {
-    outline: none;
-    border-color: #777;
-}
-
-.toggle-container {
-    display: flex;
-    align-items: center;
-}
-
-.toggle-switch {
-    position: relative;
-    width: 36px;
-    height: 16px;
-    margin-right: 10px;
-}
-
-.toggle-switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-}
-
-.toggle-slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    transition: .4s;
-    border-radius: 16px;
-}
-
-.toggle-slider:before {
-    position: absolute;
-    content: "";
-    height: 12px;
-    width: 12px;
-    left: 2px;
-    bottom: 2px;
-    background-color: white;
-    transition: .4s;
-    border-radius: 50%;
-}
-
-input:checked + .toggle-slider {
-    background-color: #4CAF50;
-}
-
-input:focus + .toggle-slider {
-    box-shadow: 0 0 1px #4CAF50;
-}
-
-input:checked + .toggle-slider:before {
-    transform: translateX(20px);
-}
-
-.toggle-slider:before {
-    position: absolute;
-    content: "";
-    height: 12px;
-    width: 12px;
-    left: 2px;
-    bottom: 2px;
-    background-color: white;
-    transition: .4s;
-    border-radius: 50%;
-}
-
-input:checked + .toggle-slider:before {
-    transform: translateX(16px);
-}
-
-.result-box {
-    position: relative;
-    background-color: #4CAF50; /* Green color */
-    color: white;
-    padding: 20px;
-    border-radius: 10px;
-    text-align: center;
-    margin-top: 20px;
-}
-
-#result-content {
-    padding-right: 24px; /* Adjust the padding as needed */
-}
-
-.close-icon {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    cursor: pointer;
-}
-
-/* Footer styles */
-footer {
-    background-color: #222;
-    color: #ccc;
-    text-align: center;
-    padding: 10px 0;
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    z-index: 1000; /* Ensure the footer is above other content */
-    margin-top: -15%; /* Adjust margin to fully display the footer */
-}
-/* Media queries for responsiveness */
-@media only screen and (max-width: 768px) {
-    .container {
-        padding: 10px;
+// Function to switch between tabs
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-    .logo-text {
-        font-size: 20px;
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    .button-container button {
-        padding: 8px 16px;
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+// Function to toggle the display of general speedups input fields
+function toggleGeneralSpeedups(tabName) {
+    var form = document.getElementById(tabName + "_form");
+    var generalSpeedupsContainer = document.getElementById(tabName + "_general_speedups");
+    var button = document.getElementById(tabName + "_general_speedups_toggle");
+
+    if (!button) {
+        console.error("Button element not found.");
+        return;
     }
-    /* Adjust footer position */
-    footer {
-        position: static;
+
+    if (button.checked) {
+        // Add general speedups inputs
+        var generalSpeedups = [
+            { name: "general_1m", label: "1m Speedup" },
+            { name: "general_5m", label: "5m Speedup" },
+            { name: "general_15m", label: "15m Speedup" },
+            { name: "general_1h", label: "1h Speedup" },
+            { name: "general_3h", label: "3h Speedup"},
+            { name: "general_8h", label: "8h Speedup" }
+        ];
+
+        generalSpeedups.forEach(speedup => {
+            var input = document.createElement("input");
+            input.type = "number";
+            input.id = speedup.name;
+            input.name = speedup.name;
+            input.value = "0";
+            input.required = true;
+
+            var label = document.createElement("label");
+            label.htmlFor = speedup.name;
+            label.textContent = speedup.label;
+
+            var div = document.createElement("div");
+            div.classList.add("input-group");
+            div.appendChild(label);
+            div.appendChild(input);
+
+            generalSpeedupsContainer.appendChild(div);
+        });
+    } else {
+        // Clear any existing general speedups
+        generalSpeedupsContainer.innerHTML = "";
+    }
+}
+
+// Function to calculate the total skip time
+function calculate(tabName) {
+    // Retrieve elements
+    var resultBox = document.getElementById("result-box");
+    var form = document.getElementById(tabName + "_form");
+    var generalSpeedupsToggle = document.getElementById(tabName + "_general_speedups_toggle");
+
+    // Initialize total minutes
+    var totalMinutes = 0;
+
+    // Retrieve input elements
+    const minutesInput = form.elements[tabName + '_1m'];
+    const fiveMinutesInput = form.elements[tabName + '_5m'];
+    const fifteenMinutesInput = form.elements[tabName + '_15m'];
+    const oneHourInput = form.elements[tabName + '_1h'];
+    const threeHourInput = form.elements[tabName + '_3h'];
+    const eightHoursInput = form.elements[tabName + '_8h'];
+
+    // Calculate minutes for each speedup type
+    totalMinutes += parseInt(minutesInput.value) || 0;
+    totalMinutes += (parseInt(fiveMinutesInput.value) || 0) * 5;
+    totalMinutes += (parseInt(fifteenMinutesInput.value) || 0) * 15;
+    totalMinutes += (parseInt(oneHourInput.value) || 0) * 60;
+    totalMinutes += (parseInt(threeHourInput.value) || 0) * 180;
+    totalMinutes += (parseInt(eightHoursInput.value) || 0) * 480;
+
+    // Add general speedups time if enabled
+    if (generalSpeedupsToggle.checked) {
+        const generalSpeedupMinutesValues = [1, 5, 15];
+        for (let value of generalSpeedupMinutesValues) {
+            const generalSpeedupInput = form.elements["general_" + value + "m"];
+            if (generalSpeedupInput && !isNaN(parseInt(generalSpeedupInput.value))) {
+                totalMinutes += parseInt(generalSpeedupInput.value) * value;
+            }
+        }
+        const generalSpeedupHoursValues = [1, 3, 8];
+        for (let value of generalSpeedupHoursValues) {
+            const generalSpeedupInput = form.elements["general_" + value + "h"];
+            if (generalSpeedupInput && !isNaN(parseInt(generalSpeedupInput.value))) {
+                totalMinutes += parseInt(generalSpeedupInput.value) * value * 60;
+            }
+        }
+    }
+
+    // Convert minutes to days, hours, and minutes
+    var days = Math.floor(totalMinutes / (24 * 60));
+    var hours = Math.floor((totalMinutes % (24 * 60)) / 60);
+    var minutes = totalMinutes % 60;
+
+    // Display the result
+    resultBox.innerHTML = `
+        <span class="close-icon" onclick="closeResultBox()">
+            <svg class="close-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M18 6L6 18M6 6l12 12"></path>
+            </svg>
+        </span>
+        Total: ${days} days, ${hours} hours, ${minutes} minutes
+    `;
+    resultBox.style.display = "block";
+}
+
+// Function to close the result box
+function closeResultBox() {
+    const resultBox = document.getElementById("result-box");
+    resultBox.style.display = "none";
+}
+
+// Function to clear all input fields
+function clearFields(tabName) {
+    var form = document.getElementById(tabName + "_form");
+    var inputs = form.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type === "number") {
+            inputs[i].value = "0";
+        }
     }
 }
